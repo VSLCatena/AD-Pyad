bda7ae4309b01d8a1d27b3de9dcf310f1aca9418
Martie <martijn9873@hotmail.com>
2017-08-06 23:30:05 +0200

    Fixed bugs and extra functionality
    check of enabled/disabled accounts
    removed errors with strings
    when user is not member anymore move based on en/disabled acc
    added wait function to see summary

--- Python_script_AD.py
+++ Python_script_AD.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+
 #
 #import
 #
 from pyad import *
 from unidecode import unidecode
 import ConfigParser
+
+
 
 #
 #settings
@@ -14,23 +17,23 @@ import ConfigParser
 config = ConfigParser.SafeConfigParser()
 config.read('config.cfg')
 
+filename = config.get('general','filename')
 ou_base = config.get('ou', 'base')
 ou_disabled = config.get('ou','disabled')
-
+ou_reunisten = config.get('ou','reunisten')
 user_pw=config.get('user','pw')
 user_domain=config.get('user','domain')
 
 
 #connect to AD
 #
-#read CSV
 #
+#  read CSV
 # ['name','username','descr',email']
 #
 import csv
-c=0
 la_users=[]
-with open('FILENAME.CSV', 'rb') as f:
+with open(filename, 'rb') as f:
  reader = csv.reader(f,delimiter = ';')
  #for row in reader:
   #print row[0][0]
@@ -49,7 +52,7 @@ la_users_name=[bb[0] for bb in la_users]
 import pyad.adquery
 q = pyad.adquery.ADQuery()
 q.execute_query(
-    attributes = [ "description", "displayName", "mail", "sAMAccountName"],
+    attributes = [ "description", "displayName", "mail", "sAMAccountName","userAccountControl"],
     where_clause = "objectClass = 'user'",
     base_dn = ou_base
 )
@@ -59,11 +62,17 @@ for x in q.get_results():
  ad_user_data.append(x["displayName"])
  ad_user_data.append(x["sAMAccountName"])
  ad_user_data.append(x["mail"])
+ ad_user_data.append(x["description"])
+ ad_user_data.append(x["userAccountControl"])
  ad_users.append(ad_user_data)
 ad_users_name=[xx[0] for xx in ad_users]
-#print ad_users_name
 
- 
+
+
+#
+# Actions
+# 
+
 def create(name,un,desc=None,mail=None,en=False,pw=user_pw,domain=user_domain ):
  "create (if not in AD and in LA)"
  ou_test = pyad.adcontainer.ADContainer.from_dn(ou_base)
@@ -88,15 +97,28 @@ def update( user1,desc1,mail1):
   
  return 
    
-def move( user1):
- "Move (if user in AD and not in LA)"
+def move( user1,enabled):
+ "Move (if user in AD and not in LA) to reunisten if user has been enabled before else to lid af"
+ 
+ # Acc enabled or not (userAccountControl)
+ # 512=Enabled
+ # 514= Disabled
+ # 66048 = Enabled, password never expires
+ # 66050 = Disabled, password never expires
+ 
  user = pyad.aduser.ADUser.from_cn(user1,ou_base)
  try:
-  ou_disabled = pyad.adcontainer.ADContainer.from_dn(ou_disabled)
-  user.move(ou_disabled)
+  if enabled == 514 or 66050:
+   ou = pyad.adcontainer.ADContainer.from_dn(ou_disabled)
+   user.move(ou)
+  
+  if enabled == 512 or 66048:
+   ou = pyad.adcontainer.ADContainer.from_dn(ou_reunisten)
+   user.move(ou)   
+     
  except Exception as e:
   print e
- #ERROR: (-2147352567, 'Exception occurred.', (0, u'Active Directory', u'There is no such object on the server.\r\n', None, 0, -2147016656), None)
+ 
  return
 
 
@@ -104,15 +126,16 @@ def move( user1):
 # 
 # Set functions  
 #
+
  
 #create set
 s=set(la_users_name)
 t=set(ad_users_name)
 
 #do set
-c = s - t #create based on LA
-m = t - s #move based on AD
-e = s & t #edit based on LA
+c = s - t #create based on LA - new member
+m = t - s #move based on AD - old member
+e = s & t #edit based on LA - current member
 
 #create list from set
 list_create=list(c)
@@ -124,31 +147,33 @@ print "Created: ",len(c)
 print "Moved: ",len(m)
 print "Edited: ",len(e)
 
-# for y in list_move:
-# move_disabled(y)
+
+#
+# Execute functions after question (ctrl c to quit)
+#
 
 
-
+raw_input("Continue? ") 
 for a in la_users:
  for b in list_create:
   if b==a[0]: 
    try:
-    create(a[0],a[1],a[2],a[3])
+	create(a[0],a[1],a[2],a[3])
    except Exception as e:
-    print e
+	print e
 	
  for c in list_edit:
   if c==a[0]: 
    try:
-    edit(a[0],a[2],a[3])
+	update(a[0],a[2],a[3])
    except Exception as e:
-    print e	
+	print e	
 
 for a in ad_users:
  for b in list_move:
   if b==a[0]: 
    try:
-    move(a[0])
+	move(a[0],a[4])
    except Exception as e:
-    print e
+	print e
 	
